ba76de9cb39f728a859e614d3516a3d468b8f5fe
wedexyz <38834987+wedexyz@users.noreply.github.com>
2022-02-03 01:53:48 +0700

    Add files via upload

---
+++ sound.js
@@ -0,0 +1,11 @@
+var x = document.getElementById("myAudio"); 
+var x1 = document.getElementById("myAudio1"); 
+var x2 = document.getElementById("myAudio2"); 
+function playAudio() {  x.play(); } 
+function pauseAudio() { x.pause(); } 
+
+function playAudio1() {   x1.play();  } 
+function pauseAudio1() {  x1.pause(); } 
+
+function playAudio2() {   x2.play();  } 
+function pauseAudio2() {  x2.pause(); } 
